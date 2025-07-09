0e5fdb07110bc3f257f53ee47a2266ee328c16ee
DESKTOP-12PLBUK\Lenovo <truonganhkietdev@gmail.com>
2025-07-09 22:56:10 +0700

    implement getLeaveTypeListRequestHandler

--- HR.LeaveManagement.Application/Features/LeaveTypes/Handlers/Queries/GetLeaveTypeListRequestHandler.cs
+++ HR.LeaveManagement.Application/Features/LeaveTypes/Handlers/Queries/GetLeaveTypeListRequestHandler.cs
@@ -1,5 +1,7 @@
-﻿using HR.LeaveManagement.Application.DTOs;
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.DTOs;
 using HR.LeaveManagement.Application.Features.LeaveTypes.Requests;
+using HR.LeaveManagement.Application.Persistence.Contracts;
 using MediatR;
 using System;
 using System.Collections.Generic;
@@ -11,9 +13,17 @@ namespace HR.LeaveManagement.Application.Features.LeaveTypes.Handlers.Queries
 {
     public class GetLeaveTypeListRequestHandler : IRequestHandler<GetLeaveTypeListRequest, List<LeaveTypeDto>>
     {
-        public Task<List<LeaveTypeDto>> Handle(GetLeaveTypeListRequest request, CancellationToken cancellationToken)
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+        private readonly IMapper _mapper;
+        public GetLeaveTypeListRequestHandler(ILeaveTypeRepository leaveTypeRepository, IMapper mapper)
         {
-            throw new NotImplementedException();
+            _leaveTypeRepository = leaveTypeRepository;
+            _mapper = mapper;
+        }
+        public async Task<List<LeaveTypeDto>> Handle(GetLeaveTypeListRequest request, CancellationToken cancellationToken)
+        {
+            var leaveTypes = await _leaveTypeRepository.GetAll();
+            return _mapper.Map<List<LeaveTypeDto>>(leaveTypes);
         }
     }
 }
