091a2fd43b2e4f7c4670488bf17116545cec22b2
DESKTOP-12PLBUK\Lenovo <truonganhkietdev@gmail.com>
2025-07-09 22:52:02 +0700

    handson implement CQRS

---
+++ HR.LeaveManagement.Application/Features/LeaveTypes/Handlers/Queries/GetLeaveTypeListRequestHandler.cs
@@ -0,0 +1,19 @@
+﻿using HR.LeaveManagement.Application.DTOs;
+using HR.LeaveManagement.Application.Features.LeaveTypes.Requests;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Features.LeaveTypes.Handlers.Queries
+{
+    public class GetLeaveTypeListRequestHandler : IRequestHandler<GetLeaveTypeListRequest, List<LeaveTypeDto>>
+    {
+        public Task<List<LeaveTypeDto>> Handle(GetLeaveTypeListRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
